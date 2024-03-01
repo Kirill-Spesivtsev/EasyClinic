52bfd921b1c74acac6248808bbe6dc143a165faa
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-03-01 23:08:30 +0300

    feat: add user register endpoint in IdentytyService

---
+++ src/AuthService/EasyClinic.AuthService.Application/Commands/RegisterUserCommandHandler.cs
@@ -0,0 +1,62 @@
+﻿using EasyClinic.AuthService.Application.DTO;
+using EasyClinic.AuthService.Domain.Entities;
+using EasyClinic.AuthService.Domain.Exceptions;
+using EasyClinic.AuthService.Application.Services;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace EasyClinic.AuthService.Application.Commands
+{
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, UserToReturnDto>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ITokenService _tokenService;
+
+        public RegisterUserCommandHandler(
+            UserManager<ApplicationUser> userManager,
+            ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        public async Task<UserToReturnDto> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+           var user = new ApplicationUser
+            {
+                Email = request.Email,
+                UserName = request.Email,
+            };
+
+            var userCheck = await _userManager.FindByEmailAsync(request.Email);
+
+            if (userCheck != null)
+            {
+                throw new ConflictException("User with such email already exists");
+            }
+
+            var register = await _userManager.CreateAsync(user, request.Password);
+
+            if (request.Password != request.RepeatPassword) 
+            { 
+                throw new BadRequestException("Passwords do not match");
+            } 
+
+            if (!register.Succeeded) 
+            { 
+                throw new BadRequestException("Invalid data provided");
+            } 
+
+            var result = new UserToReturnDto
+            {
+                Id = user.Id,
+                Email = user.Email!,
+                Token = _tokenService.GenerateToken(user),
+            };
+
+            return result;
+        }
+
+    }
+}
