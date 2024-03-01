69a74776196736a048e0a0f246a4343bfdbf9767
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-03-01 08:08:15 +0300

    feat: configure db connection and identity in AuthService

--- src/AuthService/EasyClinic.AuthService.Api/Program.cs
+++ src/AuthService/EasyClinic.AuthService.Api/Program.cs
@@ -10,6 +10,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication();
+
+builder.Services.AddDbContext<IdentityServiceDbContext>(options =>
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:IdentityServiceConnection"])
+);
+
+builder.Services.AddIdentityCore<ApplicationUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequiredLength = 6;
+        
+    })
+    .AddEntityFrameworkStores<IdentityServiceDbContext>()
+    .AddSignInManager<SignInManager<ApplicationUser>>();
 
 var app = builder.Build();
 
