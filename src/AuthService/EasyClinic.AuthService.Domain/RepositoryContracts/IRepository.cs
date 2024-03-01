d7b78eb686bbbf7397fbefa9acc8e801e5e27aaf
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-03-01 16:55:28 +0300

    feat: add generic repository for AuthService

---
+++ src/AuthService/EasyClinic.AuthService.Domain/RepositoryContracts/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyClinic.AuthService.Domain.RepositoryContracts
+{
+    public interface IRepository<T>
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+
+        public Task<T> AddAsync(T entity);
+
+        public Task DeleteAsync(T entity);
+
+        public Task<T?> GetByIdAsync(Guid id);
+
+        public Task UpdateAsync(T entity);
+    }
+}
