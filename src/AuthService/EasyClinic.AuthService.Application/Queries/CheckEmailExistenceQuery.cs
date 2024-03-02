5420d4007e9232c54133397c697a5cc0be3e8383
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-03-02 08:10:46 +0300

    feat: add email existence check endpoint

---
+++ src/AuthService/EasyClinic.AuthService.Application/Queries/CheckEmailExistenceQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+
+namespace EasyClinic.AuthService.Application.Queries
+{
+    public record CheckEmailExistenceQuery : IRequest<bool> 
+    {
+        public string Email { get;set; } = default!;
+    }
+
+}
---
+++ src/AuthService/EasyClinic.AuthService.Application/Queries/CheckEmailExistenceQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using EasyClinic.AuthService.Domain.Entities;
+using EasyClinic.AuthService.Domain.RepositoryContracts;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyClinic.AuthService.Application.Queries
+{
+    internal class CheckEmailExistenceQueryHandler : IRequestHandler<CheckEmailExistenceQuery, bool>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public CheckEmailExistenceQueryHandler(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<bool> Handle(CheckEmailExistenceQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _userManager.FindByEmailAsync(request.Email);
+            return result != null;
+        }
+    }
+}
