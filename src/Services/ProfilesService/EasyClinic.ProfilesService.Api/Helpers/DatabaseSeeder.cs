181d2fc3363d658607d64ce9cff18c727febcd42
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-04-08 10:55:57 +0300

    feat: configured all services in Api project of ProfilesService

---
+++ src/Services/ProfilesService/EasyClinic.ProfilesService.Api/Helpers/DatabaseSeeder.cs
@@ -0,0 +1,57 @@
+﻿using EasyClinic.ProfilesService.Domain.Entities;
+using EasyClinic.ProfilesService.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EasyClinic.ProfilesService.Api.Helpers
+{
+    /// <summary>
+    /// Helper class to automatically apply migrations 
+    /// and seed initial table data on application startup.
+    /// </summary>
+    public static class DatabaseSeeder
+    {
+        /// <summary>
+        /// Migrates pending schema changes to database.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static async Task SeedData(WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ProfilesServiceDbContext>();
+            await context.Database.MigrateAsync();
+            await SeedTables(context);
+        }
+
+        /// <summary>
+        /// Seeds EmployeeStatuses and MedicalSpecializations table rows.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private static async Task SeedTables(ProfilesServiceDbContext context)
+        {
+            string[] employeeStatuses = { "At work", "On vacation", "Sick Day", "Sick Leave", "Self-isolation", "Leave without pay", "Inactive" };
+            var existingEmployeeStatuses = await context.EmployeeStatuses.Select(r => r.Name).ToListAsync();
+            
+            foreach (var name in employeeStatuses.Except(existingEmployeeStatuses))
+            {
+                context.EmployeeStatuses.Add(new EmployeeStatus{ Name = name });
+            }
+
+            string[] medicalSpecializations = { "Therapist" };
+            var existingmedicalSpecializations = await context.MedicalSpecializations.Select(r => r.Name).ToListAsync();
+            
+            foreach (var name in medicalSpecializations.Except(existingmedicalSpecializations))
+            {
+                context.MedicalSpecializations.Add(new MedicalSpecialization{ Name = name });
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Services/ProfilesService/EasyClinic.ProfilesService.Api/Program.cs
@@ -0,0 +1,127 @@
+using EasyClinic.ProfilesService.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using EasyClinic.ProfilesService.Domain.Entities;
+using EasyClinic.ProfilesService.Api.Helpers;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using EasyClinic.ProfilesService.Domain.Exceptions;
+using Hellang.Middleware.ProblemDetails;
+using EasyClinic.ProfilesService.Infrastructure.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using System.Globalization;
+using EasyClinic.ProfilesService.Application.Commands;
+using EasyClinic.ProfilesService.Domain.Contracts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter JWT Token",
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+builder.Services.AddAuthentication();
+
+builder.Services.AddProblemDetails(options =>
+{
+    options.IncludeExceptionDetails = (ctx, ex) =>
+        (builder.Environment.IsDevelopment() || builder.Environment.IsStaging()) 
+        && ex is not HttpResponseCodeException;
+    options.ShouldLogUnhandledException = (_, ex, d) => 
+        (builder.Environment.IsDevelopment() || builder.Environment.IsStaging())
+        && (d.Status is null or >= 400);
+        options.Map<HttpResponseCodeException>(ex => new ProblemDetails() { 
+        Title = ex.Title, Detail = ex.Message, Status = ex.Status, Type = ex.Type });
+        options.MapToStatusCode<Exception>(500);
+});
+
+builder.Services.AddDbContext<ProfilesServiceDbContext>(options =>
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:ProfilesServiceConnection"])
+);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtOrigin:Issuer"],
+            ValidAudience = builder.Configuration["JwtOrigin:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+                builder.Configuration["JwtOrigin:Key"]!))
+        };
+    });
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("ApiCorsPolicy", policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("http://localhost:*", "https://localhost:*");
+    });
+});
+
+builder.Services.AddFluentValidationAutoValidation(op => 
+    op.DisableDataAnnotationsValidation = true)
+    .AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<CreatePatientProfileCommand>();
+ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("en-GB");
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreatePatientProfileCommand>());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("ApiCorsPolicy");
+
+app.UseProblemDetails();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DatabaseSeeder.SeedData(app);
+
+app.Run();
