dd2609b355ebcf10e810893ba5d7474dc196e6e7
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-04-08 11:19:43 +0300

    feat: configure ProfilesService Infrastructure

---
+++ src/Services/ProfilesService/EasyClinic.ProfilesService.Infrastructure/Migrations/20240404144205_Initial.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyClinic.ProfilesService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicalSpecializations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicalSpecializations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatientProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccountId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientProfiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReceptionistProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OfficeId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccountId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceptionistProfiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DoctorProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SpecializationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OfficeId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CareerStartYear = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccountId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoctorProfiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DoctorProfiles_EmployeeStatuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "EmployeeStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DoctorProfiles_MedicalSpecializations_SpecializationId",
+                        column: x => x.SpecializationId,
+                        principalTable: "MedicalSpecializations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoctorProfiles_SpecializationId",
+                table: "DoctorProfiles",
+                column: "SpecializationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoctorProfiles_StatusId",
+                table: "DoctorProfiles",
+                column: "StatusId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DoctorProfiles");
+
+            migrationBuilder.DropTable(
+                name: "PatientProfiles");
+
+            migrationBuilder.DropTable(
+                name: "ReceptionistProfiles");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeStatuses");
+
+            migrationBuilder.DropTable(
+                name: "MedicalSpecializations");
+        }
+    }
+}
---
+++ src/Services/ProfilesService/EasyClinic.ProfilesService.Infrastructure/ProfilesServiceDbContext.cs
@@ -0,0 +1,47 @@
+﻿using EasyClinic.ProfilesService.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Reflection;
+
+namespace EasyClinic.ProfilesService.Infrastructure
+{
+    /// <summary>
+    /// Database context for ProfilesService DB.
+    /// </summary>
+    public class ProfilesServiceDbContext : DbContext
+    {
+        public ProfilesServiceDbContext(DbContextOptions<ProfilesServiceDbContext> options) 
+            : base(options)
+        {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DoctorProfile>().HasKey(d => d.Id);
+            modelBuilder.Entity<PatientProfile>().HasKey(p => p.Id);
+            modelBuilder.Entity<ReceptionistProfile>().HasKey(r => r.Id);
+
+            modelBuilder.Entity<DoctorProfile>()
+                .HasOne(d => d.Specialization)
+                .WithMany();
+
+            modelBuilder.Entity<DoctorProfile>()
+                .HasOne(d => d.Status)
+                .WithMany();
+        }
+
+        public DbSet<PatientProfile> PatientProfiles { get; set; }
+
+        public DbSet<DoctorProfile> DoctorProfiles { get; set; }
+
+        public DbSet<ReceptionistProfile> ReceptionistProfiles { get; set; }
+
+        public DbSet<MedicalSpecialization> MedicalSpecializations { get; set; }
+
+        public DbSet<EmployeeStatus> EmployeeStatuses { get; set; }
+
+    }
+}
