475ab84b106b3ee425257b556b7946302d35a551
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-05-07 09:08:00 +0300

    refactor: ProfilesService projects cleanup

--- src/Services/ProfilesService/EasyClinic.ProfilesService.Infrastructure/Repositories/DoctorProfilesRepository.cs
+++ src/Services/ProfilesService/EasyClinic.ProfilesService.Infrastructure/Repositories/DoctorProfilesRepository.cs
@@ -1,14 +1,8 @@
 ﻿using EasyClinic.ProfilesService.Domain.Contracts;
 using EasyClinic.ProfilesService.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EasyClinic.ProfilesService.Infrastructure.Repository
 {
