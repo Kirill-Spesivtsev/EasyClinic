4d71b17e13a83162e06c795d6b39d5b68f2a973f
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-05-19 20:00:12 +0300

    feat: add EmailPatternService in AppointmentsService

---
+++ src/Services/AppointmentsService/EasyClinic.AppointmentsService.Application/Services/EmailPatternService.cs
@@ -0,0 +1,69 @@
+﻿using EasyClinic.AppointmentsService.Domain.Entities;
+using KEmailSender;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EasyClinic.AppointmentsService.Application.Services
+{
+    /// <summary>
+    /// Class to consctruct patterns for email messages.
+    /// Implementation of <see cref="IEmailPatternService">. 
+    /// </summary>
+    public class EmailPatternService : IEmailPatternService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IEmailSender _emailSender;
+        public EmailPatternService(IConfiguration configuration,
+            IEmailSender emailSender)
+        {
+            _configuration = configuration;
+            _emailSender = emailSender;
+        }
+
+        /// <summary>
+        /// Creates email pattern to send appointment results to user email.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public async Task SendAppointmentResultsEmail(AppointmentResult appointmentResult, DateOnly date, TimeOnly time, string email, string username)
+        {
+            var message = new EmailMessageModel
+            {
+                Subject = $"Here are your appointment results for {date}, {time}",
+                Content = $"<span style='font-family: Arial, sans-serif;'><p>Hi, {username}</p>"
+                    + $"<p>Thank you fou using our medical services!</p></span>",
+                From = _configuration["EmailSender:Username"]!,
+                To = new List<string>{email}
+            };
+
+            await _emailSender.SendEmailAsync(message);
+        }
+
+        /// <summary>
+        /// Creates email pattern to send appointment results to user email.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public async Task SendAppointmentReminderEmail(DateOnly date, TimeOnly time, string email, string username)
+        {
+            var message = new EmailMessageModel
+            {
+                Subject = "Tomorrow doctor appointment reminder",
+                Content = $"<span style='font-family: Arial, sans-serif;'><p>Hi, {username}</p>"
+                    + $"<p>You have an appointment tomorrow({date}) at {time}.</p>"
+                    + $"<p>Thank you fou using our medical services!</p></span>",
+                From = _configuration["EmailSender:Username"]!,
+                To = new List<string>{email}
+            };
+
+            await _emailSender.SendEmailAsync(message);
+        }
+
+    }
+}
---
+++ src/Services/AppointmentsService/EasyClinic.AppointmentsService.Application/Services/IEmailPatternService.cs
@@ -0,0 +1,26 @@
+﻿using EasyClinic.AppointmentsService.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace EasyClinic.AppointmentsService.Application.Services
+{
+    /// <summary>
+    /// Interface to consctuct patterns for email messages.
+    /// </summary>
+    public interface IEmailPatternService
+    {
+        /// <summary>
+        /// Constructs email pattern for appointment results.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public Task SendAppointmentResultsEmail(AppointmentResult appointmentResult, DateOnly date, TimeOnly time, string email, string username);
+
+        /// <summary>
+        /// Constructs email pattern for appointment reminder.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public Task SendAppointmentReminderEmail(DateOnly date, TimeOnly time, string email, string username);
+    }
+}
